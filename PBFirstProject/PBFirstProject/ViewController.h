1c1410ac3d597b59bde1b4694fe9a35d9e888d6c
lpb <lpb@itacast.cn>
2015-08-03 11:34:50 +0800

    初始化项目

---
+++ PBFirstProject/PBFirstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PBFirstProject
+//
+//  Created by 刘鹏博 on 15/8/3.
+//  Copyright (c) 2015年 LiuPengBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PBFirstProject/PBFirstProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PBFirstProject
+//
+//  Created by 刘鹏博 on 15/8/3.
+//  Copyright (c) 2015年 LiuPengBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
